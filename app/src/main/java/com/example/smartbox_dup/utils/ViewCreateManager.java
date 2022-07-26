d3ea191a5b7557e382106566e45b74515acc1773
unknown <chanyb0827@gmail.com>
2022-07-26 10:37:41 +0900

    add: (ViewCreateManager) createView 오버로딩함수 추가, ImageView 생성할 수 있도록 추가

--- app/src/main/java/com/example/smartbox_dup/utils/ViewCreateManager.java
+++ app/src/main/java/com/example/smartbox_dup/utils/ViewCreateManager.java
@@ -1,10 +1,16 @@
 package com.example.smartbox_dup.utils;
 
 import android.content.Context;
+import android.media.Image;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.smartbox_dup.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,25 +22,48 @@ public class ViewCreateManager {
     private ViewCreateManager () {
         savedViews = new JSONObject();
     }
-    public enum ViewType {Button, TextView}
-    public enum LayoutType {ConstraintLayout}
+    public enum ViewType {BUTTON, TEXTVIEW, IMAGEVIEW}
+    public enum LayoutType {CONSTRAINTLAYOUT}
     private JSONObject savedViews;
+    private View view;
+    ViewGroup.LayoutParams layoutParams;
 
     public static ViewCreateManager getInstance() {
         return instance;
     }
 
 
-    public String createView(Context context, ViewType viewType, ViewGroup layout, ViewGroup.LayoutParams layoutParams) {
-        View view;
+    /*
+     * (1/2) createView - LayoutType을 선택하면, 기본으로 layoutParams을 주고 View를 생성하는 함수
+     */
+    public String createView(Context context, ViewType viewType, ViewGroup layout, LayoutType layoutType) {
+
+        switch (layoutType) {
+            case CONSTRAINTLAYOUT:
+                layoutParams = new ConstraintLayout.LayoutParams(0,0);
+                ((ConstraintLayout.LayoutParams) layoutParams).matchConstraintPercentWidth = 0.9f;
+                ((ConstraintLayout.LayoutParams) layoutParams).matchConstraintPercentHeight = 0.1f;
+                ((ConstraintLayout.LayoutParams) layoutParams).topToTop = layout.getId();
+                ((ConstraintLayout.LayoutParams) layoutParams).bottomToBottom = layout.getId();
+                ((ConstraintLayout.LayoutParams) layoutParams).startToStart = layout.getId();
+                ((ConstraintLayout.LayoutParams) layoutParams).endToEnd = layout.getId();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + layoutType);
+        }
 
         switch (viewType) {
-            case Button:
+            case BUTTON:
                 view = new Button(context);
+                ((Button) view).setText("new Button");
                 break;
-            case TextView:
+            case TEXTVIEW:
                 view = new TextView(context);
                 ((TextView) view).setText("new TextView");
+                break;
+            case IMAGEVIEW:
+                view = new ImageView(context);
+                ((ImageView) view).setBackgroundResource(R.drawable.example);
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + viewType);
@@ -51,7 +80,42 @@ public class ViewCreateManager {
         }
         saveView(id, view);
         return id;
+    }
 
+    /*
+     * (2/2) createView - 사전에 정의한 LayoutParams을 인자로 받아 View에 적용해주는 함수
+     */
+    public String createView(Context context, ViewType viewType, ViewGroup layout, ViewGroup.LayoutParams layoutParams) {
+
+
+        switch (viewType) {
+            case BUTTON:
+                view = new Button(context);
+                ((Button) view).setText("new Button");
+                break;
+            case TEXTVIEW:
+                view = new TextView(context);
+                ((TextView) view).setText("new TextView");
+                break;
+            case IMAGEVIEW:
+                view = new ImageView(context);
+                ((ImageView) view).setBackgroundResource(R.drawable.example);
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + viewType);
+        }
+
+        view.setLayoutParams(layoutParams);
+        layout.addView(view);
+
+        String id = null;
+        try {
+            id = String.valueOf(DatetimeManager.getInstance().getUTCDateTime().get("timestamp"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        saveView(id, view);
+        return id;
     }
 
     private void saveView(String id,View view) {
