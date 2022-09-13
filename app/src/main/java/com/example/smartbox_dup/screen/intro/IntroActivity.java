b5660e6c70fc1b429c5b5605574406567009281a
unknown <chanyb0827@gmail.com>
2022-09-13 17:23:16 +0900

    add: (intro) introActivity, resource

---
+++ app/src/main/java/com/example/smartbox_dup/screen/intro/IntroActivity.java
@@ -0,0 +1,60 @@
+package com.example.smartbox_dup.screen.intro;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smartbox_dup.R;
+import com.example.smartbox_dup.broadcastreceiver.BroadcastManager;
+import com.example.smartbox_dup.broadcastreceiver.NetworkBroadcastReceiver;
+import com.example.smartbox_dup.screen.login.LoginActivity;
+import com.example.smartbox_dup.utils.GlobalApplcation;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkpoints();
+        nextPage();
+    }
+
+    private void nextPage() {
+        Intent nextPageIntent = new Intent(this, LoginActivity.class);
+        startActivity(nextPageIntent);
+        finish();
+    }
+
+    private void checkpoints() {
+        // 브로드캐스트 등록
+        NetworkBroadcastReceiver networkBroadcastReceiver = new NetworkBroadcastReceiver();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        BroadcastManager.getInstance().register(networkBroadcastReceiver, intentFilter);
+
+        // foreground/background 상태 리스너 등록
+        setGroundStateListener();
+    }
+
+    private void setGroundStateListener() {
+        GlobalApplcation.getContext().addListener(new GlobalApplcation.Listener() {
+            @Override
+            public void onBecameForeground() {
+                Log.i("this", "onBecameForeground");
+            }
+            @Override
+            public void onBecameBackground() {
+                Log.i("this", "onBecameBackground");
+            }
+        });
+    }
+}
