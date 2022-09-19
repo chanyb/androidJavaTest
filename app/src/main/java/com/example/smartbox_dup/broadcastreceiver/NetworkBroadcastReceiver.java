ff10c456f44483943e4013ccc0710cb51ae34295
unknown <chanyb0827@gmail.com>
2022-09-19 10:15:29 +0900

    update: (log) networkBroadcastReceiver log 추가

--- app/src/main/java/com/example/smartbox_dup/broadcastreceiver/NetworkBroadcastReceiver.java
+++ app/src/main/java/com/example/smartbox_dup/broadcastreceiver/NetworkBroadcastReceiver.java
@@ -8,14 +8,18 @@ import android.util.Log;
 
 
 public class NetworkBroadcastReceiver extends BroadcastReceiver {
+    private String TAG = "NetworkBroadcastReceiver";
 
     @Override
     public void onReceive(Context mContext, Intent intent) {
         ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
         if(connectivityManager.getActiveNetworkInfo()==null) {
             // network disconnected
+            Log.i(TAG, "Network disconnected");
         } else if (connectivityManager.getActiveNetworkInfo()!= null && connectivityManager.getActiveNetworkInfo().isConnected()) {
+            Log.i(TAG, "Network connected");
         } else {
+            Log.i(TAG, "UNKNOWN");
         }
     }
 }
