da22ea38986fdfc307cde60e8a145aa27c4052b5
unknown <chanyb0827@gmail.com>
2022-09-20 10:33:28 +0900

    add: (function) 기능 페이지 생성

---
+++ app/src/main/java/com/example/smartbox_dup/screen/function/FunctionListActivity.java
@@ -0,0 +1,16 @@
+package com.example.smartbox_dup.screen.function;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smartbox_dup.R;
+
+public class FunctionListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function);
+    }
+}
